37a3bd40fb87224a63f03692f82e639a41f64d98
Brian Foster <bfoster6@butlercc.edu>
2020-10-14 13:20:58 -0500

    Initial Commit

---
+++ Good Times/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Good Times
+//
+//  Created by Professor Foster on 10/14/20.
+//  Copyright © 2020 Professor Foster. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
